a7f6e1f34b9527be6c8f3438684ff1a9dc9c34cf
karam4u <160854629+karam4u@users.noreply.github.com>
2024-03-08 08:15:59 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+// using operators and show answer of every question 8
+// addition
+var num1 = 4;
+var num2 = 4;
+var sum = num1 + num2;
+console.log(sum);
+// substriction
+var num3 = 16;
+var num4 = 8;
+var sub = num3 - num4;
+console.log(sub);
+// multipication
+var num5 = 2;
+var num6 = 4;
+var mul = num5 * num6;
+console.log(mul);
+// divide
+var num7 = 64;
+var num8 = 8;
+var div = num7 / num8;
+console.log(div);
+// reminder 
+var num9 = 26;
+var num10 = 9;
+var reminder = num9 % num10;
+console.log(reminder);
---
+++ main.ts
@@ -0,0 +1,43 @@
+// using operators and show answer of every question 8
+
+// addition
+
+let num1 = 4
+let num2 = 4
+let sum = num1 + num2 
+console.log(sum)
+
+// substriction
+
+let num3 = 16
+let num4 = 8
+let sub = num3 - num4
+console.log(sub)
+
+// multipication
+
+let num5 = 2
+let num6 = 4
+let mul = num5 * num6
+console.log(mul)
+
+// divide
+
+let num7 = 64
+let num8 = 8
+let div = num7 / num8
+console.log(div)
+
+// reminder 
+
+let num9 = 26
+let num10 = 9
+let reminder = num9 % num10
+console.log(reminder)
+
+//    done 
+// all answer is equal to 8 
+
+
+
+
